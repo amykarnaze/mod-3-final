3893d9a2ab0b3a2b1f4d18b61b28d573da2de9b7
Amy Karnaze <akarnaze@gmail.com>
2020-09-22 10:05:05 -0600

    Add imports and exp data

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,1 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@testing-library/jest-dom';
+import { screen, render, waitFor, fireEvent } from '@testing-library/react';
+import App from './App';
+jest.mock('../../apiCalls.js')
+import { postOrder } from '../../apiCalls';
 
+
+
+{
+  "orders": [{
+    "id": 1,
+    "name": "Pat",
+    "ingredients": ["beans", "lettuce", "carnitas", "queso fresco", "jalapeno"]
+  }, {
+    "id": 2,
+    "name": "Sam",
+    "ingredients": ["steak", "pico de gallo", "lettuce", "carnitas", "queso fresco", "jalapeno"]
+  }, {
+    "id": 3,
+    "name": "Alex",
+    "ingredients": ["sofritas", "beans", "sour cream", "carnitas", "queso fresco"]
+  }]
+}
---
+++ src/components/OrderForm/OrderForm.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@testing-library/jest-dom';
+import { screen, render, waitFor, fireEvent } from '@testing-library/react';
+import OrderForm from './OrderForm';
